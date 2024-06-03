8262d5f1362a3f04e2c307757eec96b7eab3ea48
israt6898 <israt6898@gmail.com>
2024-06-03 23:09:52 +0600

    post opreation create

--- index.js
+++ index.js
@@ -31,15 +31,30 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
+
+     
   try {
+
+    const bloodRequestCollection= client.db('bloodLife').collection('bloodRequest');
+
+
+    //createDonationform data save
+    
+    app.post('/blood', async(req,res)=>{
+      const bloodData= req.body;
+      const result = await bloodRequestCollection.insertOne(bloodData);
+      res.send(result);
+    })
+
+
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
